dc59d85e48be5f2dd73ca3e8af2c6968ba39d9b2
Prince Yadav <princebhaipk0048@gmail.com>
2025-08-13 13:57:40 +0000

    > nextn@0.1.0 db:generate > drizzle-kit generate
    No config path provide

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,7 +8,7 @@ if (!process.env.DATABASE_URL) {
 export default defineConfig({
   schema: "./src/lib/schema.ts",
   out: "./drizzle",
-  driver: "pg",
+  dialect: "postgresql",
   dbCredentials: {
     connectionString: process.env.DATABASE_URL,
   },
